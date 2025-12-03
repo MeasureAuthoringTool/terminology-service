3a3c4ecdd462162ed98aa6a97d35aa6f3ca34d5f
sb-bdonohue <brendan.donohue@semanticbits.com>
2025-12-03 17:05:20 -0500

    MAT-9247 add oit domains to cors settings

--- src/main/java/gov/cms/madie/terminology/TerminologyServiceApplication.java
+++ src/main/java/gov/cms/madie/terminology/TerminologyServiceApplication.java
@@ -31,6 +31,9 @@ public class TerminologyServiceApplication {
                 "https://dev-madie.hcqis.org",
                 "https://test-madie.hcqis.org",
                 "https://impl-madie.hcqis.org",
+		"https://dev.madie.internal.cms.gov",
+                "https://test.madie.internal.cms.gov",
+                "https://impl.madie.internal.cms.gov",
                 "https://madie.cms.gov");
       }
     };
