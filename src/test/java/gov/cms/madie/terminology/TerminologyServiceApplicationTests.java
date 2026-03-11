7194639e5389927a6dc3f99c9765bdf3b76e2f45
Joseph Kotanchik <joseph.kotanchik@semanticbits.com>
2026-03-11 11:33:33 -0400

    MAT-9672: Adding mongock created a dependency on @SpringBootTest classes to have a mongoTemplate instance. Applying application-test config to disable Mongock during tests.

--- src/test/java/gov/cms/madie/terminology/TerminologyServiceApplicationTests.java
+++ src/test/java/gov/cms/madie/terminology/TerminologyServiceApplicationTests.java
@@ -2,9 +2,12 @@ package gov.cms.madie.terminology;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 public class TerminologyServiceApplicationTests {
+
   @Test
   void contextLoads() {}
 }
--- src/test/java/gov/cms/madie/terminology/task/ManifestCacheEvictJobTest.java
+++ src/test/java/gov/cms/madie/terminology/task/ManifestCacheEvictJobTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.CacheManager;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ManifestCacheEvictJobTest {
 
