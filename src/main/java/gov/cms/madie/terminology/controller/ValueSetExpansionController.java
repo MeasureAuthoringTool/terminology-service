3b660bf4fbd61b47585a42914b52670be9c49288
Joseph Kotanchik <joseph.kotanchik@semanticbits.com>
2026-04-24 11:59:15 -0400

    MAT-9749: Match existing admin endpoint paths (/terminology/admin).

--- src/main/java/gov/cms/madie/terminology/controller/ValueSetExpansionController.java
+++ src/main/java/gov/cms/madie/terminology/controller/ValueSetExpansionController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 import java.util.Set;
 
 @RestController
-@RequestMapping(path = "/admin/terminology")
+@RequestMapping(path = "/terminology/admin")
 @Slf4j
 @RequiredArgsConstructor
 public class ValueSetExpansionController {
